42550992121a3438c5c69fea9156fca370c8cac5
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-12-16 17:00:23 +0100

    add astro & siimple

---
+++ siimple.config.js
@@ -0,0 +1,10 @@
+import noir from "@siimple/preset-noir";
+
+export default {
+    ...noir,
+    // ...other configuration
+    styles: {
+        ...noir.styles,
+        // other custom styles
+    },
+};
