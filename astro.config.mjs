5a38000f882925360a9221dff87877c087032c6f
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-12-17 00:52:40 +0100

    refactor astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'astro/config';
 
+const SERVER_PORT = 4321
+
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://rodielmj.github.io"
+
+const _npmScript = process.env.npm_lifecycle_script || "";
+
+const isBuild = _npmScript.includes("astro build");
+let BASE_URL = LOCALHOST_URL
+
+if (isBuild) {
+    BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
-    // site: "https://github.com/Rodielm/rodielmj.github.io",
-    // base: '/rodielmj.github.io'
-    
+    sever: { port: SERVER_PORT },
+    site: BASE_URL
 });
