04f972657378d67578b5c1898d57a959dd7bf763
Rodiel Martinez <10471758+Rodielm@users.noreply.github.com>
2023-12-16 17:41:51 +0100

    modify astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://github.com/Rodielm/rodielmj.github.io",
+    // site: "https://github.com/Rodielm/rodielmj.github.io",
     // base: '/rodielmj.github.io'
 });
